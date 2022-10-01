5f7fead0bce1ad1196931089991f1f7ff739346a
CloverSoncyou <110799952+CloverSoncyou@users.noreply.github.com>
2022-10-01 22:37:22 +0900

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-let cookies=0; //開始時のクッキー枚数
-
-      function audio() {
-    document.getElementById('click_audio').currentTime = 0; //連続クリックに対応
-    document.getElementById('click_audio').play(); //クリックしたら音を再生
-}
