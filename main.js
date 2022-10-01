6a0ff7522a9b5bac2950b2a10852725726955176
CloverSoncyou <110799952+CloverSoncyou@users.noreply.github.com>
2022-10-01 16:19:51 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let cookies=0; //開始時のクッキー枚数
+
+      function audio() {
+    document.getElementById('click_audio').currentTime = 0; //連続クリックに対応
+    document.getElementById('click_audio').play(); //クリックしたら音を再生
+}
